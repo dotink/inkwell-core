8917c64543b4f43812a9b3d591d59bb04b2201ea
Matthew J. Sahagian <msahagian@dotink.org>
2014-10-13 23:52:24 -0700

    Add base controller class

---
+++ src/Controller.php
@@ -0,0 +1,77 @@
+<?php namespace Inkwell
+{
+	use ArrayAccess;
+	use Dotink\Flourish;
+	use IW;
+
+	class Controller implements ArrayAccess
+	{
+		/**
+		 *
+		 */
+		private $context = array();
+
+
+		/**
+		 * Sets a context element via array access (NOT ALLOWED)
+		 *
+		 * @access public
+		 * @param mixed $offset The context element offset to set
+		 * @param mixed $value The value to set for the offset
+		 * @return void
+		 */
+		public function offsetSet($offset, $value)
+		{
+			$this->context[$offset] = $value;
+		}
+
+
+		/**
+		 * Checks whether or not a context element exists
+		 *
+		 * @access public
+		 * @param mixed $offset The context element offset to check for existence
+		 * @return boolean TRUE if the context exists, FALSE otherwise
+		 */
+		public function offsetExists($offset)
+		{
+			return isset($this->context[$offset]);
+		}
+
+
+		/**
+		 * Attempts to unset a context element
+		 *
+		 * @access public
+		 * @param mixed $offset The context element offset to unset
+		 * @return void
+		 */
+		public function offsetUnset($offset)
+		{
+			if ($this->offsetExists($offset)) {
+				unset($this->context[$offset]);
+			}
+		}
+
+
+		/**
+		 * Gets a context element
+		 *
+		 * @access public
+		 * @param mixed $offset The context element offset to get
+		 * @return mixed The value of the offset
+		 */
+		public function offsetGet($offset)
+		{
+			if (!$this->offsetExists($offset)) {
+				throw new Flourish\ProgrammerException(
+					'Provider "%s" not set on parent %s',
+					$offset,
+					__CLASS__
+				);
+			}
+
+			return $this->context[$offset];
+		}
+	}
+}
